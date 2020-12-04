db5ea55dd8201884b088ded7f1bf3e42658b5989
Anton Strand <anton.strand@insurello.se>
2020-12-04 09:47:56 +0100

    Solve day 4 task 1

---
+++ day4a.fsx
@@ -0,0 +1,37 @@
+(*
+    Find "valid" passports.
+*)
+
+open System.IO
+
+let input = File.ReadAllText "./day4-input.txt"
+
+let requiredCategories =
+    Set.ofList [ "byr"
+                 "iyr"
+                 "eyr"
+                 "hgt"
+                 "hcl"
+                 "ecl"
+                 "pid" ]
+
+let split (chars: char array) (string: string) = string.Split chars
+
+let pickCategories =
+    split [| ' '; '\n' |]
+    >> Array.map (split [| ':' |] >> Array.head)
+    >> Set.ofArray
+
+let toPassports (str: string) =
+    str.Split "\n\n"
+    |> List.ofArray
+    |> List.map pickCategories
+
+let isValidPassport = Set.isSubset requiredCategories
+
+let solve =
+    toPassports
+    >> List.where isValidPassport
+    >> List.length
+
+solve input |> printfn "Solution %A"
