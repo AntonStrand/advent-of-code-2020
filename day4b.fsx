6441dbad436d7c20ff6b24d1d031e11282a4dbe7
Anton Strand <anton.strand@insurello.se>
2020-12-06 12:38:36 +0100

    Solve day2b with parser combinators

--- day4b.fsx
+++ day4b.fsx
@@ -10,143 +10,120 @@ open Parser
 
 let input = File.ReadAllText "./day4-input.txt"
 
+let always x _ = x
+
 let whitespace = many (anyOf [ ' '; '\n'; '\t' ])
 
 let digit = anyOf [ '0' .. '9' ]
 
-let byr19 =
-    sequence [ pchar '1'
-               pchar '9'
-               anyOf [ '2' .. '9' ]
-               digit ]
-
-let byr20 =
-    sequence [ pchar '2'
-               pchar '0'
-               pchar '0'
-               anyOf [ '0' .. '2' ] ]
-
-let pByr =
-    choice [ byr19; byr20 ]
+let byr =
+    pstring "byr"
+    .>> pchar ':'
+    .>> choice [ (pstring "19"
+                  .>>. sequence [ anyOf [ '2' .. '9' ]; digit ])
+                 (pstring "200"
+                  .>>. (anyOf [ '0' .. '2' ] |>> List.singleton)) ]
     .>> whitespace
-    |> noRemainingInput
-    |>> charListToString
 
-let iyr10 =
-    sequence [ pchar '1'; digit ] |>> charListToString
-
-let iyr20 = pstring "20"
-
-let pIyr =
-    sequence [ iyr20
-               choice [ iyr10; iyr20 ] ]
+let iyr =
+    pstring "iyr"
+    .>> pstring ":20"
+    .>> choice [ sequence [ pchar '1'; digit ] |>> charListToString
+                 pstring "20" ]
     .>> whitespace
-    |> noRemainingInput
-    |>> List.reduce (+)
 
-let eyr20 =
-    sequence [ pchar '2'; digit ] |>> charListToString
-
-let eyr30 = pstring "30"
-
-let pEyr =
-    pstring "20"
-    .>>. choice [ eyr20; eyr30 ]
+let eyr =
+    pstring "eyr"
+    .>> pstring ":20"
+    .>> choice [ sequence [ pchar '2'; digit ] |>> charListToString
+                 pstring "30" ]
     .>> whitespace
-    |>> fun (h, d) -> h + d
 
 let cm =
     pchar '1'
-    .>>. (sequence [ anyOf [ '5' .. '8' ]; digit ]
-          <|> sequence [ pchar '9'
-                         anyOf [ '0' .. '3' ] ])
-    .>>. pstring "cm"
-    |>> fun ((h, ds), cm) -> charListToString (h :: ds) + cm
+    >>. (sequence [ anyOf [ '5' .. '8' ]; digit ]
+         <|> sequence [ pchar '9'
+                        anyOf [ '0' .. '3' ] ])
+    >>. pstring "cm"
 
 let inch =
     choice [ sequence [ pchar '5'; pchar '9' ]
              sequence [ pchar '6'; digit ]
              sequence [ pchar '7'
                         anyOf [ '0' .. '6' ] ] ]
-    .>>. pstring "in"
-    |>> fun (ds, inch) -> charListToString ds + inch
+    >>. pstring "in"
 
-let pHgt =
-    choice [ cm; inch ]
+let hgt =
+    pstring "hgt"
+    .>> pchar ':'
+    .>> choice [ cm; inch ]
     .>> whitespace
-    |> noRemainingInput
+
 
 let hex = anyOf ([ '0' .. '9' ] @ [ 'a' .. 'f' ])
 
-let pHcl =
-    sequence (pchar '#' :: List.replicate 6 hex)
+let hcl =
+    pstring "hcl"
+    .>> sequence (pchar ':' :: pchar '#' :: List.replicate 6 hex)
     .>> whitespace
-    |> noRemainingInput
-    |>> charListToString
 
-let pEcl =
-    [ "amb"
-      "blu"
-      "brn"
-      "gry"
-      "grn"
-      "hzl"
-      "oth" ]
-    |> List.map pstring
-    |> choice
-    |> noRemainingInput
-
-
-let pPid =
-    sequence (List.replicate 9 digit)
+let ecl =
+    pstring "ecl"
+    .>> pchar ':'
+    .>> ([ "amb"
+           "blu"
+           "brn"
+           "gry"
+           "grn"
+           "hzl"
+           "oth" ]
+         |> List.map pstring
+         |> choice)
     .>> whitespace
+
+let pid =
+    pstring "pid"
+    .>> pchar ':'
+    .>> sequence (List.replicate 9 digit)
+    .>> whitespace
+
+let cid =
+    pstring "cid"
+    .>> pchar ':'
+    .>> many1 digit
+    .>> whitespace
+
+let toPassports (str: string) = str.Split "\n\n" |> List.ofArray
+
+let parsePassport =
+    choice [ byr
+             iyr
+             eyr
+             hgt
+             hcl
+             ecl
+             pid
+             cid ]
+    |> many
     |> noRemainingInput
-    |>> charListToString
 
-let categoryValidation =
-    [ ("byr", pByr)
-      ("iyr", pIyr)
-      ("eyr", pEyr)
-      ("hgt", pHgt)
-      ("hcl", pHcl)
-      ("ecl", pEcl)
-      ("pid", pPid) ]
-
-let split delimiter (string: string) = string.Split delimiter
-
-let pickCategories =
-    split [| ' '; '\n' |]
-    >> Array.map
-        (split [| ':' |]
-         >> (fun category -> (Array.head category, Array.last category)))
-    >> Map.ofArray
-
-let toPassports (str: string) =
-    str.Split "\n\n"
-    |> List.ofArray
-    |> List.map pickCategories
-
-let traverse f list =
-    list
-    |> List.fold (fun mAcc x ->
-        mAcc
-        |> Option.bind (fun acc ->
-            match f x with
-            | Some y -> Some(y :: acc)
-            | None -> None)) (Some [])
+let requiredCategories =
+    Set.ofList [ "byr"
+                 "iyr"
+                 "eyr"
+                 "hgt"
+                 "hcl"
+                 "ecl"
+                 "pid" ]
 
 let isValidPassport passport =
-    categoryValidation
-    |> traverse (fun (key, parser) ->
-        Map.tryFind key passport
-        |> Option.bind (fun category ->
-            match run parser category with
-            | Success v -> Some v
-            | Failure _ -> None))
+    match run parsePassport passport with
+    | Success (parsed, _) -> Set.isSubset requiredCategories (Set.ofList parsed)
+    | Failure _ -> false
 
 let solve =
     toPassports
-    >> List.choose isValidPassport
+    >> List.where isValidPassport
     >> List.length
 
 solve input |> printfn "Solution %A"
